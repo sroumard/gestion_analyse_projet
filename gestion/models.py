a3b160fae9a50344207ee44d446570584b6462dd
sroumard <s.roumard@gmail.com>
2024-11-05 14:02:40 +0100

    Initial commit

---
+++ gestion/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Projet(models.Model):
+    nom = models.CharField(max_length=200)
+    description = models.TextField()
+    date_debut = models.DateField()
+    date_fin = models.DateField(null=True, blank=True)
+    statut = models.CharField(max_length=50, choices=[('En cours', 'En cours'), ('Terminé', 'Terminé'), ('En attente', 'En attente')])
+
+    def __str__(self):
+        return self.nom
+
+class Tache(models.Model):
+    projet = models.ForeignKey(Projet, on_delete=models.CASCADE, related_name='taches')
+    titre = models.CharField(max_length=200)
+    description = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_echeance = models.DateField(null=True, blank=True)
+    statut = models.CharField(max_length=50, choices=[('À faire', 'À faire'), ('En cours', 'En cours'), ('Terminé', 'Terminé')])
+
+    def __str__(self):
+        return self.titre
